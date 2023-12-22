39a3a3ab53f62e2a96c8d2950ed72091a6570386
John1Souza <johnatas2014@hotmail.com>
2023-12-22 10:02:12 -0300

    [NEW] First commit

---
+++ src/main/java/com/john/minefield/MinefieldApplication.java
@@ -0,0 +1,13 @@
+package com.john.minefield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinefieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinefieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/john/minefield/MinefieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.minefield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinefieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
